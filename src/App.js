f947b908726562c4d862389a74f800fc3ecb0d08
ADMIN <ADMIN@DESK^Cgit config --global user.email ADMIN@DESK^C>
2022-08-01 00:09:26 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Public } from './Modules/Public/Public';
+import { Admin } from './Modules/Admin/Admin';
+import { Sidebar } from './Modules/Shared/Sidebar/Sidebar';
+import { Navbar } from './Modules/Shared/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar />
+      <Public />
+      <Admin />
+    </>
+
   );
 }
 
